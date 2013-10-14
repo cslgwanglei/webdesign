38aac86ac95208f6d7fdac503913e752a6b6d9db
cslgwanglei <595234608@qq.com>
2013-10-14 18:56:19 +0800

    webdesign modify
    I have modify the webdesign ，It will better than the last

--- js/webdesign.js
+++ js/webdesign.js
@@ -1,5 +1,6 @@
 $(function(){
 	photoshopopen();
+	mousexy();
 });
  
 function photoshopopen(){
@@ -11,4 +12,17 @@ function photoshopopen(){
 	 	{$(this).next("div").css('display','none');};
 	 })
 
+}
+
+function mousexy(imurl,a,b){
+	var x=$(".containerbig").clientX;
+
+	$(".containerbig").css('top',y-b);
+	$(".containerbig").css('left',x-a);
+	$(".containerbig").css("background-image","url('../images/29812-bcss.jpg)");
+	$(".containerbig").css('display','block');
+	$(".containerbig").css('width',a);
+	$(".containerbig").css('height',b);
+
+alert(x);
 }
